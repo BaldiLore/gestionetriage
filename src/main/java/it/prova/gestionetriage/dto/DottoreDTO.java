f846b3494022f9005d33eb7d6d8f6e90248f05a2
BaldiLore <baldilorenzo2600@gmail.com>
2021-11-23 14:41:40 +0100

    Commit creazione dto

---
+++ src/main/java/it/prova/gestionetriage/dto/DottoreDTO.java
@@ -0,0 +1,86 @@
+package it.prova.gestionetriage.dto;
+
+
+import it.prova.gestionetriage.model.Dottore;
+
+public class DottoreDTO {
+
+	private Long id;
+	private String nome;
+	private String cognome;
+	private String codiceDipendente;
+
+	private PazienteDTO pazienteAttualmenteInVisita;
+
+	public DottoreDTO() {
+		super();
+	}
+
+	public DottoreDTO(String nome, String cognome, String codiceDipendente, PazienteDTO pazienteAttualmenteInVisita) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		this.codiceDipendente = codiceDipendente;
+		this.pazienteAttualmenteInVisita = pazienteAttualmenteInVisita;
+	}
+
+	public DottoreDTO(Long id, String nome, String cognome, String codiceDipendente,
+			PazienteDTO pazienteAttualmenteInVisita) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.codiceDipendente = codiceDipendente;
+		this.pazienteAttualmenteInVisita = pazienteAttualmenteInVisita;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getCodiceDipendente() {
+		return codiceDipendente;
+	}
+
+	public void setCodiceDipendente(String codiceDipendente) {
+		this.codiceDipendente = codiceDipendente;
+	}
+
+	public PazienteDTO getPazienteAttualmenteInVisita() {
+		return pazienteAttualmenteInVisita;
+	}
+
+	public void setPazienteAttualmenteInVisita(PazienteDTO pazienteAttualmenteInVisita) {
+		this.pazienteAttualmenteInVisita = pazienteAttualmenteInVisita;
+	}
+	
+	public Dottore buildDottoreModel() {
+		return new Dottore(this.id, this.nome, this.cognome, this.codiceDipendente,
+				this.pazienteAttualmenteInVisita.buildPazienteModel());
+	}
+
+	public static DottoreDTO buildDottoreDTOFromModel(Dottore input) {
+		return new DottoreDTO(input.getId(), input.getNome(), input.getCognome(), input.getCodiceDipendente(),
+				PazienteDTO.buildPazienteDTOFromModel(input.getPazienteAttualmenteInVisita()));
+	}
+}
---
+++ src/main/java/it/prova/gestionetriage/dto/PazienteDTO.java
@@ -0,0 +1,118 @@
+package it.prova.gestionetriage.dto;
+
+import java.util.Date;
+
+import it.prova.gestionetriage.model.Paziente;
+import it.prova.gestionetriage.model.StatoPaziente;
+
+public class PazienteDTO {
+
+	private Long id;
+	private String nome;
+	private String cognome;
+	private String codiceFiscale;
+	private Date dataRegistrazione;
+	private StatoPaziente statoPaziente;
+	private DottoreDTO dottore;
+
+	public PazienteDTO() {
+		super();
+	}
+
+	public PazienteDTO(String nome, String cognome, String codiceFiscale, Date dataRegistrazione,
+			StatoPaziente statoPaziente, DottoreDTO dottore) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		this.codiceFiscale = codiceFiscale;
+		this.dataRegistrazione = dataRegistrazione;
+		this.statoPaziente = statoPaziente;
+		this.dottore = dottore;
+	}
+
+	public PazienteDTO(Long id, String nome, String cognome, String codiceFiscale, Date dataRegistrazione,
+			StatoPaziente statoPaziente, DottoreDTO dottore) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.codiceFiscale = codiceFiscale;
+		this.dataRegistrazione = dataRegistrazione;
+		this.statoPaziente = statoPaziente;
+		this.dottore = dottore;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getCodiceFiscale() {
+		return codiceFiscale;
+	}
+
+	public void setCodiceFiscale(String codiceFiscale) {
+		this.codiceFiscale = codiceFiscale;
+	}
+
+	public Date getDataRegistrazione() {
+		return dataRegistrazione;
+	}
+
+	public void setDataRegistrazione(Date dataRegistrazione) {
+		this.dataRegistrazione = dataRegistrazione;
+	}
+
+	public StatoPaziente getStatoPaziente() {
+		return statoPaziente;
+	}
+
+	public void setStatoPaziente(StatoPaziente statoPaziente) {
+		this.statoPaziente = statoPaziente;
+	}
+
+	public DottoreDTO getDottore() {
+		return dottore;
+	}
+
+	public void setDottore(DottoreDTO dottore) {
+		this.dottore = dottore;
+	}
+
+	@Override
+	public String toString() {
+		return "PazienteDTO [id=" + id + ", nome=" + nome + ", cognome=" + cognome + ", codiceFiscale=" + codiceFiscale
+				+ ", dataRegistrazione=" + dataRegistrazione + ", statoPaziente=" + statoPaziente + "]";
+	}
+
+	public Paziente buildPazienteModel() {
+		return new Paziente(this.id, this.nome, this.cognome, this.codiceFiscale, this.dataRegistrazione,
+				this.statoPaziente, this.dottore.buildDottoreModel());
+	}
+
+	public static PazienteDTO buildPazienteDTOFromModel(Paziente input) {
+		return new PazienteDTO(input.getId(), input.getNome(), input.getCognome(), input.getCodiceFiscale(),
+				input.getDataRegistrazione(), input.getStatoPaziente(),
+				DottoreDTO.buildDottoreDTOFromModel(input.getDottore()));
+	}
+
+}
